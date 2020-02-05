48fb0bb12e4e677edc304116d7a875aac9a52843
Enfioz <G00387876@gmit.ie>
2020-02-05 00:06:43 +0000

    Weekly task 3 - reverse string

---
+++ secondstring.py
@@ -0,0 +1,13 @@
+# Patrick Corcoran
+# Weekly Task 3 - strings
+
+# Write a program that asks a user to input
+# a string and outputs every second letter
+# in reverse order
+
+s = "The quick brown fox jumps over the lazy dog."
+
+s = (input("Please enter a sentence: "))
+
+print(s[::-1][0:44:2])
+
